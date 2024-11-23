b83fdd39430bc14c816320e233ffb94535c90974
raonicerqueira1234 <raoni.cerqueira@revolck.com.br>
2024-11-23 02:49:08 -0300

    feat(auth): Adicionando middleware para checar leveis de acesso dos usuários.

---
+++ src/middlewares/middleware_roles/rolesMiddleware.js
@@ -0,0 +1,27 @@
+const authorization = {
+    checkRole: (role) => {
+        return (req, res, next) => {
+            if (!req.user || req.user.role_id !== role) {
+                return res.status(403).json({ error: `Access denied: This resource is for ${role}s only` });
+            }
+            next();
+        };
+    },
+
+    admin: (req, res, next) => authorization.checkRole(4)(req, res, next),
+    company: (req, res, next) => authorization.checkRole(3)(req, res, next),
+    student: (req, res, next) => authorization.checkRole(2)(req, res, next),
+    teacher: (req, res, next) => authorization.checkRole(1)(req, res, next),
+
+    accessLevel: (requiredLevel) => {
+        return (req, res, next) => {
+            const level = req.user.role_level;
+            if (level < requiredLevel) {
+                return res.status(403).json({ error: `Access denied: You need level ${requiredLevel} or higher to access this resource.` });
+            }
+            next();
+        }
+    }
+};
+
+module.exports = authorization;
