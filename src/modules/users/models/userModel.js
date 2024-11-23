f3d2baa49eb71230a253391c9c76136c57e6885e
raonicerqueira1234 <raoni.cerqueira@revolck.com.br>
2024-11-23 20:47:11 -0300

    fix(model): mudança da tabela para user]

--- src/modules/users/models/userModel.js
+++ src/modules/users/models/userModel.js
@@ -3,20 +3,20 @@ const knexInstance = require('../../../config/db');
 const userModel = {
     create: async (user) => {
         const db = await knexInstance();
-        const [id] = await db('users').insert(user);
+        const [id] = await db('user').insert(user);
         return id;
     },
     findByEmail: async (email) => {
         const db = await knexInstance();
-        return db('users').where({ email }).first();
+        return db('user').where({ email }).first();
     },
     findByCpf: async (cpf) => {
         const db = await knexInstance();
-        return db('users').where({ cpf }).first();
+        return db('user').where({ cpf }).first();
     },
     findById: async (id) => {
         const db = await knexInstance();
-        return db('users').where({ id }).first();
+        return db('user').where({ id }).first();
     },
     findByLevel: async (role_id) => {
         const db = await knexInstance();
@@ -24,11 +24,11 @@ const userModel = {
     },
     update: async (id, user) => {
         const db = await knexInstance();
-        return db('users').where({ id }).update(user);
+        return db('user').where({ id }).update(user);
     },
     delete: async (id) => {
         const db = await knexInstance();
-        return db('users').where({ id }).del();
+        return db('user').where({ id }).del();
     }
 };
 
