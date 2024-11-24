d53942eb375ea6ad6d609866c392d870e8b7afdd
raonicerqueira1234 <raoni.cerqueira@revolck.com.br>
2024-11-24 18:51:54 -0300

    feat(swagger): Documentação de rotas user

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { swaggerUi, swaggerDocs } = require('./config/swagger');
 const userRoutes = require('./modules/users/routes/userRoutes');
 const app = express();
 
 app.use(express.json());
 app.use('/api', userRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));S
 
 module.exports = app;
