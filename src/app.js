456944e305b1ba38e3ea0e10425daf5d06eccb8f
revolck-lab <dev.hubsuite@revolck.com.br>
2024-12-01 17:31:20 +0000

    instalando dependencia de cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./modules/users/routes/userRoutes');
 const app = express();
+
+// Configura o middleware CORS
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://advancemais-front.vercel.app'], // Adicione os domínios permitidos
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+}));
 
 app.use(express.json());
 app.use('/api', userRoutes);
