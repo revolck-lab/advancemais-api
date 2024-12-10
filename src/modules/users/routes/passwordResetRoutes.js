4c7675f20ee251c445123feefc8e3ea1b534b8fa
raonicerqueira1234 <raoni.cerqueira@revolck.com.br>
2024-12-10 20:34:12 -0300

    fix(routes): removendo rota duplicada.

--- src/modules/users/routes/passwordResetRoutes.js
+++ src/modules/users/routes/passwordResetRoutes.js
@@ -3,10 +3,6 @@ const recoveryController = require('../controllers/passwordRecoveryController');
 
 const router = express.Router();
 
-router.get("/reset-password", (req, res) => {
-  res.status(200).json({ message: "Welcome!!" });
-});
-
 router.post('/recovery-password', recoveryController.requestPasswordRecovery);
 router.post('/reset-password', recoveryController.resetPassword);
 
