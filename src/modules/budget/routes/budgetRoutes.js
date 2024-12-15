f4a9d81d89b5ac1fbc2ea55e6466646993a0fd5e
raonicerqueira1234 <raoni.cerqueira@revolck.com.br>
2024-12-15 00:39:07 -0300

    feat(routes): criação de rotas para budget

---
+++ src/modules/budget/routes/budgetRoutes.js
@@ -0,0 +1,13 @@
+// routes/budgetRoutes.js
+const express = require("express");
+const budgetController = require("../controllers/budgetController");
+
+const router = express.Router();
+
+router.post("/budgets", budgetController.createBudget);
+router.get("/budgets", budgetController.listBudgets);
+router.get("/budgets/:id", budgetController.getBudgetById);
+router.put("/budgets/:id", budgetController.updateBudget);
+router.delete("/budgets/:id", budgetController.deleteBudget);
+
+module.exports = router;
