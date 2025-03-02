5b372bc427c7d5de82c50e733ee542393c2a16d9
raonicerqueira1234 <raoni.cerqueira@revolck.com.br>
2025-03-02 17:53:23 -0300

    feat(): Joi da tabela subscription

--- src/modules/signatures/validators/signaturesValidator.js
+++ src/modules/signatures/validators/signaturesValidator.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const errorMessagePackage = require('./errorMessagePackage');
+const errorMessageSubscription = require('./errorMessageSubscription');
 
 const packageValidation = Joi.objet({
   name: Joi.string().min(3).max(100).required().messages({
@@ -27,4 +28,32 @@ const packageValidation = Joi.objet({
   }),
 });
 
-module.exports = packageValidation;
+const subscriptionValidation = Joi.object({
+  company_id: Joi.number().integer().required().messages({
+    'any.required': errorMessageSubscription().company_id.required,
+    'number.base': errorMessageSubscription().company_id.invalid,
+  }),
+  package_id: Joi.number().integer().required().messages({
+    'any.required': errorMessageSubscription().package_id.required,
+    'number.base': errorMessageSubscription().package_id.invalid,
+  }),
+  start_date: Joi.date().required().format('YYYY-MM-DD').messages({
+    'any.required': errorMessageSubscription().start_date.required,
+    'date.format': errorMessageSubscription().start_date.invalid,
+  }),
+  end_date: Joi.date().required().format('YYYY-MM-DD').greater(Joi.ref('start_date')).messages({
+    'any.required': errorMessageSubscription().end_date.required,
+    'date.format': errorMessageSubscription().end_date.invalid,
+    'date.greater': errorMessageSubscription().end_date.greater,
+  }),
+  status: Joi.string().required().valid('active', 'canceled', 'expired').messages({
+    'any.required': errorMessageSubscription().status.required,
+    'string.empty': errorMessageSubscription().status.empty,
+    'string.valid': errorMessageSubscription().status.invalid,
+  }),
+});
+
+module.exports = { 
+  packageValidation,
+  subscriptionValidation
+}
