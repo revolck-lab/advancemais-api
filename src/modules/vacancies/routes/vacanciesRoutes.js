3c15caf2d73ab3d7bdd9a53c1048bde3fa503af3
raonicerqueira1234 <raoni.cerqueira@revolck.com.br>
2025-03-08 06:32:05 -0300

    feat(vacancy): ajustes de privilegios nas rotas de vagas

--- src/modules/vacancies/routes/vacanciesRoutes.js
+++ src/modules/vacancies/routes/vacanciesRoutes.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 router.get('/', authorization.accessLevel(1, 2), vacanciesController.listVacancies);
 router.get('/:id', authorization.accessLevel(1, 2),vacanciesController.getVacancyDetails);
+router.post('/', authorization.accessLevel(4), vacanciesController.createVacancy);
+router.put('/:id', authorization.accessLevel(7), vacanciesController.updateVacancy);
+router.delete('/:id', authorization.accessLevel(7), vacanciesController.deleteVacancy);
 
 module.exports = router;
 
